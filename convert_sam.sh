7856beb8b50fc48e8e7ac08694df4714cc66e472
Fokke Dekker <fokke@dekker.email>
2018-11-28 15:59:38 -0800

    update files

--- convert_sam.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-species=$1
-
-cd '/media/falklabuser/LargeData/Anneliek/new_EVEs_both/species/'$1'/bam_files/'
-
-samtools view -bS ''$1'.sam' > ''$1'.bam'
-
--- family_out.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-species=$1
-
-cd '/media/falklabuser/LargeData/Anneliek/new_EVEs_both/families/new_species/out_clusters/final_distribution'
-
-cat ''$1'.csv'
-
